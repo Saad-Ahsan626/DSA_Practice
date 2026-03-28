4eb531406fb1cf9a11dc22c74693d76aaf871414
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-03-29 00:27:29 +0500

    Problem 50

---
+++ Arrays/Compute_X^N.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+double myPow(double x, int n)
+{
+
+    long binForm = n;
+    double ans = 1;
+
+    if (n < 0)
+    {
+        x = 1/x;
+        binForm = -binForm;
+    }
+
+    while (binForm > 0)
+    {
+        if (binForm % 2 == 1)
+        {
+            ans *= x;
+        }
+
+        x *= x;
+        binForm /= 2;
+    }
+
+    return ans;
+    
+}
