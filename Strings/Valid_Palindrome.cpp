a195bd0c6100a1007d3be779de694cd0fd02c8d8
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-03-30 01:32:15 +0500

    Problem 125

---
+++ Strings/Valid_Palindrome.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+bool isAlphaNum(char c)
+{
+    if ((c >= '0' && c <= '9') || (tolower(c) >= 'a' && tolower(c) <= 'z'))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+        
+}
+
+bool isPalindrome(string s)
+{
+    int start = 0;
+    int end = s.length() - 1;
+
+    while (start < end)
+    {
+        if (!isAlphaNum(s[start]))
+        {
+            start++;
+            continue;
+        }
+        
+        if (!isAlphaNum(s[end]))
+        {
+            end--;
+            continue;
+        }
+
+        if (tolower(s[start]) != tolower(s[end]))
+        {
+            return false;
+        }
+        
+        start++;
+        end--;
+    
+    }
+    
+    return true;
+ }
