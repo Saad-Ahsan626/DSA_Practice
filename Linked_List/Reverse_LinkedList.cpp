fba136ec3f53f82800df366d07747104d98149a1
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-04-01 11:17:37 +0500

    Problem 206

---
+++ Linked_List/Reverse_LinkedList.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int data;
+    ListNode* next;
+
+    ListNode(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+ListNode* reverseList(ListNode* head) {
+    ListNode* prev = NULL;
+    ListNode* curr = head;
+    ListNode* next = NULL;
+
+    while (curr != NULL) {
+        next = curr->next;  
+        curr->next = prev;  
+        prev = curr;        
+        curr = next;
+    }
+    return prev;  
+}
