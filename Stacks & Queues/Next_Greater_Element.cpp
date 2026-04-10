ba4a9de71310f0668f2889fe731fb2fd2d29986b
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-04-10 19:56:52 +0500

    Problem 496

---
+++ Stacks & Queues/Next_Greater_Element.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<unordered_map>
+
+using namespace std;
+
+vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> map;
+        stack<int> s;
+
+        for (int i = nums2.size()-1; i >= 0; i--)
+        {
+            while (!s.empty() && s.top() <= nums2[i]) {
+                s.pop();
+            }
+            if (s.empty()) {
+                map[nums2[i]] = -1;
+            } else {
+                map[nums2[i]] = s.top();
+            }
+            s.push(nums2[i]);
+        }
+
+        vector<int> result;
+        for (int num : nums1) {
+            result.push_back(map[num]);
+        }
+        return result;
+        
+    }
