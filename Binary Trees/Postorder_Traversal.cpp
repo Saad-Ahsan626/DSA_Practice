9091496bdc79b14c56ad173079f94905aa03cb8e
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-04-14 16:23:17 +0500

    Problem 145

---
+++ Binary Trees/Postorder_Traversal.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+vector<int> postorderTraversal(TreeNode* root) {
+
+    vector<int> result;
+    if(root == nullptr)
+        return result;
+    
+    vector<int> left = postorderTraversal(root->left);
+    result.insert(result.end(), left.begin(), left.end());
+
+    vector<int> right = postorderTraversal(root->right);
+    result.insert(result.end(), right.begin(), right.end());
+
+    result.push_back(root->val);
+    
+    return result;
+}
