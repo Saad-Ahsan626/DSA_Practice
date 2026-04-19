bbab5bf8c9d137d1e4ee588b5ee150dde1a6adc8
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-04-19 19:59:05 +0500

    Problem 26

---
+++ Arrays/Remove_Duplicates_From_Sorted_Array.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) 
+{
+    if (nums.empty()) 
+    {
+        return 0;
+    }
+
+    int uniqueIndex = 0;
+    for (int i = 1; i < nums.size(); i++) 
+    {
+        if (nums[i] != nums[uniqueIndex]) 
+        {
+            uniqueIndex++;
+            nums[uniqueIndex] = nums[i];
+        }
+    }
+    return uniqueIndex + 1;
+}
