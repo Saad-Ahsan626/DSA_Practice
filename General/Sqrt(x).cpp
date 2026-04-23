e5e0be8958dccb072c4cfbad4f64cb700bb04e56
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-04-23 18:33:23 +0500

    Problem 69

---
+++ General/Sqrt(x).cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int mySqrt(int x) 
+{
+    if (x < 2) 
+    {
+        return x;
+    }
+
+    long r = x;
+   
+    while (r * r > x) 
+    {
+        r = (r + x / r) / 2;
+    }
+    
+    return (int)r;
+}
