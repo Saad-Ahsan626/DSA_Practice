6787b071137dc9b65c974d23c0137d86abc32c1f
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-05-19 22:28:18 +0500

    Problem 540

---
+++ Arrays/Single_Element_In_Sorted_Array.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int singleNonDuplicate(vector<int>& nums) 
+{
+    int low = 0, high = nums.size() - 1;
+    
+    while (low < high) 
+    {
+        int mid = low + (high - low) / 2;
+        
+        
+        if (mid % 2 == 0) 
+        {
+            
+            if (nums[mid] == nums[mid + 1]) 
+            {
+                low = mid + 2; 
+            } 
+            else 
+            {
+                high = mid; 
+            }
+        } 
+        else 
+        {
+            
+            if (nums[mid] == nums[mid - 1]) 
+            {
+                low = mid + 1; 
+            } 
+            else 
+            {
+                high = mid - 1; 
+            }
+        }
+    }
+    
+    return nums[low];          
+}
