9a19eac06f3f0c252e32593df2188e4cf00c6463
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-05-24 22:49:51 +0500

    Problem 367

---
+++ General/Valid_Perfect_Square.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+bool isPerfectSquare(int num) 
+{
+    if (num < 0) return false;
+    if (num == 0 || num == 1) return true;
+    long long left = 0, right = num;
+    
+    while (left <= right) 
+    {
+        long long mid = left + (right - left) / 2;
+        long long square = mid * mid;
+        
+        if (square == num) 
+        {
+            return true;
+        }
+        
+        if (square < num) 
+        {
+            left = mid + 1;
+        }
+        else 
+        {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
