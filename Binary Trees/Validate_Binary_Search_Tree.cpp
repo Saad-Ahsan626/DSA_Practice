9e95cc4e95a27707d95ed5c8ad2d3bbea6e05032
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-06-02 11:33:26 +0500

    Problem 98

---
+++ Binary Trees/Validate_Binary_Search_Tree.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+bool isValidBST(TreeNode* root) 
+{
+    return isValidBSTHelper(root, nullptr, nullptr);
+}
+
+bool isValidBSTHelper(TreeNode* node, TreeNode* minNode, TreeNode* maxNode) 
+{
+    if (node == nullptr) 
+    {
+        return true;
+    }
+    
+    if ((minNode != nullptr && node->val <= minNode->val) || 
+        (maxNode != nullptr && node->val >= maxNode->val)) 
+    {
+        return false;
+    }
+    
+    return isValidBSTHelper(node->left, minNode, node) && isValidBSTHelper(node->right, node, maxNode);
+}
