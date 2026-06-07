51ef1ad152ed2ad5c6503ef40b8835535268d165
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-06-07 22:04:02 +0500

    Problem 414

---
+++ Arrays/Third_Maximum_Number.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<limits.h>
+
+using namespace std;
+
+int thirdMax(vector<int>& nums) 
+{
+    long first = LONG_MIN, second = LONG_MIN, third = LONG_MIN;
+    
+    for (int num : nums) 
+    {
+        if (num == first || num == second || num == third) 
+        {
+            continue; 
+        }
+        
+        if (num > first) 
+        {
+            third = second;
+            second = first;
+            first = num;
+        } 
+        else if (num > second) 
+        {
+            third = second;
+            second = num;
+        } 
+        else if (num > third) 
+        {
+            third = num;
+        }
+    }
+    
+    return (third == LONG_MIN) ? first : third;
+}
