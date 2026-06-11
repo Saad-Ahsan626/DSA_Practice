5aadc7a270bd7813aa268bfab101bfc9ef53980d
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-06-11 23:09:52 +0500

    Problem 709

---
+++ Strings/To_Lower_Case.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string toLowerCase(string s) 
+{
+    for(int i=0; i<s.length(); i++)
+    {
+        if(s[i]>='A' && s[i]<='Z')
+        {
+            s[i] = s[i] + 32;
+        }
+    }
+    return s;        
+}
