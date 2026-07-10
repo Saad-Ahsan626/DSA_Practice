33537b3587c1cd299eced4078d781eb58c0b505c
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-07-10 12:33:31 +0500

    Problem 461

---
+++ General/Hamming_Distance.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int hammingDistance(int x, int y) {
+    int xorResult = x ^ y; 
+    int distance = 0;
+
+    
+    while (xorResult) {
+        distance += xorResult & 1; 
+        xorResult >>= 1; 
+    }
+
+    return distance;
+}
