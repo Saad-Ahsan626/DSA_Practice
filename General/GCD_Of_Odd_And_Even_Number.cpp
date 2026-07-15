73e3203b83e105fcb02b4e07f4112a3d624c5870
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-07-15 12:29:23 +0500

    Problem 3658

---
+++ General/GCD_Of_Odd_And_Even_Number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+// hahahah 
+// Since sumOdd = n^2 and sumEven = n(n+1)
+// GCD(n^2, n^2 + n) = n * GCD(n, n+1) = n * 1 = n
+
+int gcdOfOddEvenSums(int n) 
+{
+        return n;
+}
