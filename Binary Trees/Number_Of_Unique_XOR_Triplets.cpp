d8911bc7b9393517e712b77d65e9c4839db102a8
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-07-23 15:36:52 +0500

    Problem 3513

---
+++ Binary Trees/Number_Of_Unique_XOR_Triplets.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int uniqueXorTriplets(vector<int>& nums) 
+{
+    int n = nums.size();
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    return 1 << (__lg(n) + 1);
+}   
