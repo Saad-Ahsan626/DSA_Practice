b8d53f27c60a99c280322de7c0f8282b9c992b5b
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-07-24 15:13:25 +0500

    Problem 744

---
+++ Strings/Smallest_Letter_Greater_Than_Target.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+char nextGreatestLetter(vector<char>& letters, char target) 
+{
+        
+    int n = letters.size();
+    int left = 0, right = n - 1;
+    
+    while (left <= right) 
+    {
+        int mid = left + (right - left) / 2;
+        if (letters[mid] <= target) 
+        {
+            left = mid + 1;
+        } 
+        else 
+        {
+            right = mid - 1;
+        }
+    }
+    
+    return letters[left % n];
+}
+
