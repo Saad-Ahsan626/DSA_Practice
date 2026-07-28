79b4ec09d8a96865e6b3500a9529b02e37d2b6f1
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-07-28 17:58:57 +0500

    Problem 2089

---
+++ Arrays/Find_Target_Indecies_After_Sorting_Array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+vector<int> targetIndices(vector<int>& nums, int target) 
+{
+    vector<int> result;
+    sort(nums.begin(), nums.end());
+    
+    for (int i = 0; i < nums.size(); i++) 
+    {
+        if (nums[i] == target) 
+        {
+            result.push_back(i);
+        }
+    }
+    
+    return result;
+}
